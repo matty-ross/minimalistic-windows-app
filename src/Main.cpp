c4d7d8021159696d5f4f9e5941e1c5d9c91ffc1e
Matty <54020396+matty-ross@users.noreply.github.com>
2023-10-05 20:16:26 +0200

    Test program.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,14 @@
 #include <Windows.h>
 
+
+constexpr char message[] = "Hello, world!";
+constexpr DWORD messageLength = sizeof(message) - 1;
+
+
 void Start()
 {
+    HANDLE consoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    WriteConsoleA(consoleOutput, message, messageLength, nullptr, nullptr);
+
     ExitProcess(0);
 }
